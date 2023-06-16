eca9e42787118a6ce9b939948a76e3350c0f06c8
yanaih16 <51224675+yanaih16@users.noreply.github.com>
2023-06-16 18:05:08 +0900

    [add] main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+#include "first_come_first_service.hpp"
+#include "schedule.hpp"
+
+using namespace std;
+
+int main() {
+    cout << "アルゴリズム" << endl
+         << "0:到着時間方式" << endl
+         << "1:処理時間方式" << endl
+         << "2:ラウンドロビン方式" << endl;
+    int num;
+    cin >> num;
+    switch (num) {
+        case 0:
+            FirstComeFirstService schedule;
+            schedule.run();
+            schedule.result();
+            break;
+    }
+}
